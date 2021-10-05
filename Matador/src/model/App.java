dd6e20839d15a74e750cd8813be1b78eaa68abf0
Luka2477 <luke.da.duke@hotmail.com>
2021-10-06 00:21:56 +0200

    Changed Game class structure to be main JavaFX Application

---
+++ Matador/src/model/App.java
@@ -0,0 +1,9 @@
+package model;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Game.class);
+    }
+}
--- Matador/src/model/Game.java
+++ Matador/src/model/Game.java
@@ -1,13 +1,45 @@
 package model;
 
-import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game extends Application {
+    private GraphicsContext gc;
+
     private Board board;
     private ArrayList<Player> players = new ArrayList<>();
     private Die die = new Die();
+    private Scanner scanner;
 
-    public Game() {
+    @Override
+    public void start(Stage stage) {
+        GridPane root = this.initContent();
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Make Board");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private GridPane initContent() {
+        GridPane pane = new GridPane();
+        Canvas canvas = new Canvas(800, 800);
+        this.gc = canvas.getGraphicsContext2D();
+        pane.add(canvas, 0, 0);
+        this.initGame();
+        return pane;
+    }
+
+    // ------------------------------------------------------------------------
+
+    private void initGame() {
 
     }
 }
